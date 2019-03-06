95d2e1e6f287fcaf108c9530870795e4491ab7b6
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2019-03-06 15:01:24 -0600

    Integrated drizzle with our NFT contract

--- contracts/NFTStore.sol
+++ contracts/NFTStore.sol
@@ -1,30 +1,35 @@
 pragma solidity ^0.5.2;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract NFTStore is ERC721Full {
+contract NFTStore is ERC721Full, Ownable {
     using Counters for Counters.Counter;
     using SafeMath for uint256;
     Counters.Counter internal tokenId;
 
-    event LogBadgeMinted(uint256 _tokendId, address _owner);
+    event LogBadgeMinted(uint256 _tokendId, address _user);
 
     constructor(string memory name, string memory symbol)
         ERC721Full(name, symbol)
         public
-    {}
+    {
+        address payable owner = msg.sender;
+    }
 
     function mintBadge(
-        string memory tokenURI
+        string calldata tokenURI,
+        address user
     )
-        public
+        external
+        onlyOwner
         returns (bool)
     {
         uint256 badgeTokenId = tokenId.current();
         tokenId.increment();
-        _mint(msg.sender, badgeTokenId);
+        _mint(user, badgeTokenId);
         _setTokenURI(badgeTokenId, tokenURI);
-        emit LogBadgeMinted(badgeTokenId, msg.sender);
+        emit LogBadgeMinted(badgeTokenId, user);
         return true;
     }
 }
